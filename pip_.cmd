bc074b623b46bb100e5a8b1616fdafe5c1729bad
erroronline1 <dev@erroronline.one>
2021-01-06 11:20:09 +0100

    this repo might become a sort of cloud storage for my system settings

---
+++ pip_.cmd
@@ -0,0 +1,7 @@
+@echo off
+rem reinstalling a system is annoying enough. i guess i'll be happy next time to not having to find out another time why the scripts won't run.
+
+python -m pip install --upgrade pip
+pip install PILLOW
+pip install pyinstaller
+pip install pypiwin32

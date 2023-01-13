34c70dbd9a9875c9ce6f8bbdd7f2406d4b9ce9ec
erroronline1 <dev@erroronline.one>
2023-01-13 22:55:04 +0100

    added module

--- install.cmd
+++ install.cmd
@@ -33,7 +33,7 @@ if exist %cmd% (
 :: set python="C:\Program Files\Python36\python.exe" "C:\Program Files\Python38\python.exe"
 set python=py38
 
-set pip=cchardet colorama eel kivy opencv-python-headless PILLOW psutil pyinstaller pypiwin32 pyzbar requests win10toast XslxWriter fpdf qrcode wxPython
+set pip=cchardet colorama eel kivy kivymd plyer opencv-python-headless PILLOW psutil pyinstaller pypiwin32 pyzbar requests win10toast XslxWriter fpdf qrcode wxPython
 :: set pip=%pip%;buildozer
 :: remember kivy might run in a virtual environment only, so some modules might have to be reinstalled there as well
 :: same goes for wsl

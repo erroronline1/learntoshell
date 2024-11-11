d53a21dd43b70165c887c9e05e68859c51ddab3b
erroronline1 <dev@erroronline.one>
2024-11-11 23:49:56 +0100

    besser batch usage

--- 2ogg.sh
+++ 2ogg.sh
@@ -1,3 +1,18 @@
-read -p "enter file name to convert:" filename
+#!/bin/bash
 
-ffmpeg -i $filename -c:v libtheora -qscale:v 6 -c:a libvorbis -qscale:a 5 ${filename%.*}.ogv
+#read -p "enter file name to convert:" filename
+if [ "$#" -lt 1 ]
+then
+    echo "pass filenames with extension as parameters"
+    exit 1
+fi
+
+for filename in $@
+do
+    if [ -f "$filename" ]
+    then
+    ffmpeg -i $filename -c:v libtheora -qscale:v 6 -c:a libvorbis -qscale:a 5 ${filename%.*}.ogv
+    else
+    echo "$filename not found"
+    fi
+done

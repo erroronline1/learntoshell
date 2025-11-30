24b8a44df1a6a06c0f5730c30a05a46494c5e601
erroronline1 <dev@erroronline.one>
2025-11-30 01:43:22 +0100

    added python library, fixed alias path

--- install.sh
+++ install.sh
@@ -49,14 +49,14 @@ apt install python3.12-venv
 cd ~
 python3 -m venv .venv
 source .venv/bin/activate
-pip=("cchardet" "colorama" "eel" "kivy" "kivymd" "plyer" "opencv-python" "PILLOW" "psutil" "pyinstaller" "pyzbar" "requests" "xlsxwriter" "fpdf" "qrcode" "wxPython" "paramiko")
+pip=("cchardet" "colorama" "eel" "kivy" "kivymd" "plyer" "opencv-python" "PILLOW" "psutil" "pyinstaller" "pyzbar" "requests" "xlsxwriter" "fpdf" "qrcode" "wxPython" "paramiko", "qtsass")
 for lib in "${pip[@]}"
 do
     pip install "$lib"
 done
 
-alias py='source .venv/bin/activate'
-echo "alias py='source .venv/bin/activate'" >> ~/.bashrc
+alias py='source ~/.venv/bin/activate'
+echo "alias py='source ~/.venv/bin/activate'" >> ~/.bashrc
 
 tlp start
 
